7132848bcc6d21f723b8bd9155c4de011e44d597
NeginSal <neginsaljooghi@gmail.com>
2021-06-01 17:42:27 +0430

    making button and text area

---
+++ script.js
@@ -0,0 +1,8 @@
+const wording =["English texts for beginners to practice reading and comprehension online and for free. Practicing your comprehension of written English will both improve your vocabulary and understanding of grammar and word order. The texts below are designed to help you develop while giving you an instant evaluation of your progress."];
+const message = document.querySelector(".message");
+const playText = document.querySelector("textarea");
+const button = document.querySelector("button");
+button.addEventListener("click",function(){
+    console.log(this.innerText);
+
+})
