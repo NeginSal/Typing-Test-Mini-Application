63b4e8a15f62874a244b358eea8dea9e8f25a107
NeginSal <neginsaljooghi@gmail.com>
2021-06-03 18:14:22 +0430

    word by word

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
 button.addEventListener("click",function(){
-    console.log(this.innerText);
     if(this.innerText=="Start"){
         playText.disabled =false;
         playGame();
@@ -15,31 +14,44 @@ button.addEventListener("click",function(){
         endPlay();
         }
     })
+    
+//End play
 function endPlay(){
     let date = new Date();
     endTime = date.getTime();
     let totalTime =((endTime-startTime)/1000);
-    console.log(totalTime);
     let str =playText.value;
     let wordCount = wordCounter(str);
     let speed = Math.round((wordCount/totalTime)*60);
-    console.log(speed);
     let finalMessage = "you typed at"+speed+"words per minute";
+    finalMessage +=compareWords(message.innerText,str);
     message.innerHTML=finalMessage;
 }
+
+//wordCounter
 function wordCounter(strWords){
     let response =strWords.split("").length;
-    console.log(response);
     return response;
 }
 
+//compareWords
+function compareWords(str1,str2){
+    let words1 = str1.split("");
+    let words2 = str2.split("");
+    let cnt =0;
+    words1.forEach(function(item,index){
+        if(item == words2[index]){
+            cnt++;
+        }
+    })
+    return (cnt+" correct out of" +words1.length + "words ");
+}
+
+//playGame
 function playGame(){
     let randomNum =Math.floor(Math.random()*wording.length);
     message.innerText= wording[randomNum];
     let date = new Date();
     startTime = date.getTime();
-    console.log(startTime);
     button.innerText="Done";
-    console.log(randomNum);
-
 }
