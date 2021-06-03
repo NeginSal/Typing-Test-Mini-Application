d6defc0516035d596e37f510448b82fdef8fe226
NeginSal <neginsaljooghi@gmail.com>
2021-06-03 15:52:34 +0430

    Timer Date

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const wording =["English texts for beginners to practice reading and comprehension online and for free. Practicing your comprehension of written English will both improve your vocabulary and understanding of grammar and word order. The texts below are designed to help you develop while giving you an instant evaluation of your progress."];
+const wording =["English texts for beginners to practice reading and comprehension online and for free", "Practicing your comprehension of written English will both improve your vocabulary and understanding of grammar and word order.", "The texts below are designed to help you develop while giving you an instant evaluation of your progress."];
+let startTime,endTime;
 const message = document.querySelector(".message");
 const playText = document.querySelector("textarea");
 const button = document.querySelector("button");
@@ -13,6 +14,10 @@ button.addEventListener("click",function(){
 function playGame(){
     let randomNum =Math.floor(Math.random()*wording.length);
     message.innerText= wording[randomNum];
+    let date = new Date();
+    startTime = date.getTime();
+    console.log(startTime);
+    button.innerText="Done";
     console.log(randomNum);
 
 }
